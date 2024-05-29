24a2712e9845b965d43912fe07bc4922b4ee28f3
Matthias <m.geslin974@gmail.com>
2024-05-29 15:25:37 +0200

    youtube vue
    wip

---
+++ src/components/TheYoutube.vue
@@ -0,0 +1,20 @@
+<script setup>
+</script>
+
+
+<template>
+  <ContentItem>
+    <p>API Youtube Last vid</p>
+    <p>Chaine principale</p>
+    <p>Dernières vidéos
+      <iframe width="560" 
+        height="315" 
+        src="https://www.youtube.com/embed/DxVV_fpCCtI?si=0CDdZmCIpA7IX5G4" 
+        frameborder="0" 
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+        referrerpolicy="strict-origin-when-cross-origin" 
+        allowfullscreen>
+      </iframe>
+    </p>
+  </ContentItem>
+</template>
