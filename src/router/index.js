c54b2031ab9b2c12875ae553718092fc4278f9f7
Matthias <m.geslin974@gmail.com>
2024-05-29 15:22:49 +0200

    router index
    paths home/stream/youtube/links

---
+++ src/router/index.js
@@ -0,0 +1,39 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/stream',
+      name: 'stream',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/StreamView.vue')
+    },
+    {
+      path: '/youtube',
+      name: 'youtube',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/YoutubeView.vue')
+    },
+    {
+      path: '/links',
+      name: 'links',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/LinksView.vue')
+    }
+  ]
+})
+
+export default router
