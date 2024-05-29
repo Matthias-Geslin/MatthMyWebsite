6cb2ada73389b1a5bd4f77611475c01e1e9428b2
Matthias <m.geslin974@gmail.com>
2024-05-29 15:25:51 +0200

    stream vue
    wip

---
+++ src/components/TheStream.vue
@@ -0,0 +1,22 @@
+<script setup>
+</script>
+
+<template>
+  <ContentItem>
+    <p>
+      <iframe
+          src="https://player.twitch.tv/?channel=matthmoica&parent=localhost"
+          height="500"
+          width="100%"
+          frameborder="0"
+          allowfullscreen>
+      </iframe>
+    </p>
+    <p>
+      <iframe src="https://www.twitch.tv/embed/matthmoica/chat?parent=localhost"
+        height="500"
+        width="100%">
+      </iframe>
+    </p>
+  </ContentItem>
+</template>
