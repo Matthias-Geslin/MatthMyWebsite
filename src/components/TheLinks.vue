921fa8414f6d9b8dbb4342713cfa134c66e6d5b9
Matthias <m.geslin974@gmail.com>
2024-05-29 15:26:03 +0200

    links vue
    social links w/ img

---
+++ src/components/TheLinks.vue
@@ -0,0 +1,29 @@
+<template>
+  <ContentItem>
+    <p>
+      <a href="https://discord.gg/HFxq2UVeqa" target="_blank">
+        <img src="/img/Discord.png" alt="Discord Bannière">
+      </a>
+    </p>
+    <p>
+      <a href="https://x.com/matth_moi_ca" target="_blank">
+        <img src="/img/Twitter.png" alt="X/Twitter Bannière">
+      </a>
+    </p>
+    <p>
+      <a href="https://www.twitch.tv/matthmoica" target="_blank">
+        <img src="/img/Twitch.png" alt="Twitch Bannière">
+      </a>
+    </p>
+    <p>
+      <a href="https://www.youtube.com/@MatthMoiCaOfficiel" target="_blank">
+        <img src="/img/YouTube.png" alt="YouTube Bannière">
+      </a>
+    </p>
+    <p>
+      <a href="mailto:contact.matthmoica@gmail.com" target="_blank">
+        <img src="/img/Contact_pro.png" alt="Contact Pro Bannière">
+      </a>
+    </p>
+  </ContentItem>
+</template>
