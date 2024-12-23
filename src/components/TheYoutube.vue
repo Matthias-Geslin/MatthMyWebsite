80753f800575e25ba7462627609c8cc0f9d64011
Matthias <m.geslin974@gmail.com>
2024-12-23 16:40:12 +0100

    Edit
    Added a dummy  yt iframe

--- src/components/TheYoutube.vue
+++ src/components/TheYoutube.vue
@@ -4,9 +4,8 @@
 
 <template>
   <ContentItem>
-    <p>API Youtube Last vid</p>
-    <p>Chaine principale</p>
-    <p>Dernières vidéos
+    <section>
+      <h2>Youtube Last vid</h2>
       <iframe width="560" 
         height="315" 
         src="https://www.youtube.com/embed/DxVV_fpCCtI?si=0CDdZmCIpA7IX5G4" 
@@ -15,6 +14,30 @@
         referrerpolicy="strict-origin-when-cross-origin" 
         allowfullscreen>
       </iframe>
-    </p>
+    </section>
+
+    <section>
+      <h2>Chaine principale</h2>
+      <iframe width="560" 
+        height="315" 
+        src="https://www.youtube.com/embed/DxVV_fpCCtI?si=0CDdZmCIpA7IX5G4" 
+        frameborder="0" 
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+        referrerpolicy="strict-origin-when-cross-origin" 
+        allowfullscreen>
+      </iframe>
+    </section>
+    
+    <section>
+      <h2>Dernières vidéos</h2>
+      <iframe width="560" 
+        height="315" 
+        src="https://www.youtube.com/embed/DxVV_fpCCtI?si=0CDdZmCIpA7IX5G4" 
+        frameborder="0" 
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+        referrerpolicy="strict-origin-when-cross-origin" 
+        allowfullscreen>
+      </iframe>
+    </section>
   </ContentItem>
 </template>
