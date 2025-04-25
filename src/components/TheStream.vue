526f2fa4e6702633da3a644a8ff8f2d27505ee9d
Matthias <m.geslin974@gmail.com>
2025-04-25 17:01:34 +0200

    Update
    iFrame link updated with url

--- src/components/TheStream.vue
+++ src/components/TheStream.vue
@@ -5,14 +5,14 @@
   <ContentItem>
     <section>
       <iframe
-          src="https://player.twitch.tv/?channel=matthmoica&parent=localhost"
+          src="https://player.twitch.tv/?channel=matthmoica&parent=stream.matthmoiça.fr"
           height="500"
           frameborder="0"
           allowfullscreen>
       </iframe>
     </section>
     <section>
-      <iframe src="https://www.twitch.tv/embed/matthmoica/chat?parent=localhost"
+      <iframe src="https://www.twitch.tv/embed/matthmoica/chat?parent=stream.matthmoiça.fr"
         height="500">
       </iframe>
     </section>
